73dcb80850cbd02f498756e493c3f53275bd8c2d
Abhiram Rallabhandi <114180495+A-b-h-i-r-a-m@users.noreply.github.com>
2023-05-30 21:57:24 +0530

    Update Text Editor.py

--- Text Editor.py
+++ Text Editor.py
@@ -35,15 +35,15 @@ window.rowconfigure(0, minsize=800, weight=1)
 window.columnconfigure(1, minsize=800, weight=1)
 
 txt_edit = tk.Text(window)
-fr_buttons = tk.Frame(window, relief=tk.RAISED, bd=2)
-btn_open = tk.Button(fr_buttons, text="Open", command=openFile)
-btn_save = tk.Button(fr_buttons, text="Save As...", command=saveFile)
+buttons = tk.Frame(window, relief=tk.RAISED, bd=2)
+btn_open = tk.Button(buttons, text="Open", command=openFile)
+btn_save = tk.Button(buttons, text="Save As...", command=saveFile)
 
 # Arranging buttons and text area
 btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
 btn_save.grid(row=1, column=0, sticky="ew", padx=5)
 
-fr_buttons.grid(row=0, column=0, sticky="ns")
+buttons.grid(row=0, column=0, sticky="ns")
 txt_edit.grid(row=0, column=1, sticky="nsew")
 
 window.mainloop()
