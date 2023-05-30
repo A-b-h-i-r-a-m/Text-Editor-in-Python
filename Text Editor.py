b73991ea4c86f7b51c55463dee37667f3cd4f6f5
Abhiram Rallabhandi <114180495+A-b-h-i-r-a-m@users.noreply.github.com>
2023-05-30 21:46:10 +0530

    Update Text Editor.py

--- Text Editor.py
+++ Text Editor.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 from tkinter.filedialog import askopenfilename, asksaveasfilename
 
+# To open a File
 def openFile():
     
     filepath = askopenfilename(
@@ -12,8 +13,9 @@ def openFile():
     with open(filepath, "r") as input_file:
         text = input_file.read()
         txt_edit.insert(tk.END, text)
-    window.title(f"Text Editor Application - {filepath}")
+    window.title(f"Text Editor - {filepath}")
 
+# To Save a File
 def saveFile():
     
     filepath = asksaveasfilename(
@@ -25,7 +27,7 @@ def saveFile():
     with open(filepath, "w") as output_file:
         text = txt_edit.get(1.0, tk.END)
         output_file.write(text)
-    window.title(f"Text Editor Application - {filepath}")
+    window.title(f"Text Editor - {filepath}")
 
 window = tk.Tk()
 window.title("Text Editor Application")
@@ -37,6 +39,7 @@ fr_buttons = tk.Frame(window, relief=tk.RAISED, bd=2)
 btn_open = tk.Button(fr_buttons, text="Open", command=openFile)
 btn_save = tk.Button(fr_buttons, text="Save As...", command=saveFile)
 
+# Arranging buttons and text area
 btn_open.grid(row=0, column=0, sticky="ew", padx=5, pady=5)
 btn_save.grid(row=1, column=0, sticky="ew", padx=5)
 
